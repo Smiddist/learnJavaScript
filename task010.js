81f4e373fb69cc55ebb3965557c07b4985ab3117
Dmirtii <smiddist@gmail.com>
2022-08-19 18:48:48 +0300

    rework task010 and task020

--- task010.js
+++ task010.js
@@ -1,56 +1,27 @@
 
-/*
-С помощью Цикла For(), While()
-произведём 5 операций с массивом.
+       /*
+       Shift+Return (Enter) > new line
+       Arrow Up > Return the previous state of the code
+       Return (Enter) > Run the code   
+       
+    1. To know index of Мама (var x1)
+	2. Add Бабушку Дедушку (var x2)
+	2.1. Sort array alphabetically (var x3)
+	3. Exclude Кота (массив x4)
+	4. Создать строку ... - моя семья Аня, Алена, Бося, Мама, Папа (строка x5)
+	5. Вычислить длину строки (число x6)
+	6. Выводить все в консоль и проверять себя
 
-1 Создайте массив styles с элементами «Джаз» и «Блюз».
-2 Ц. Добавьте «Рок-н-ролл» в конец.
-3 Ц. Замените значение в середине на «Классика». Ваш код для поиска значения в середине должен работать для массивов с любой длиной.
-4 Удалите первый элемент массива и покажите его.
-5 Ц. Вставьте «Рэп» и «Регги» в начало массива.
-6 Ц. (while) Добавить к каждому нечетному элементу массива знак * (* Джаз)
-
-Массив по ходу выполнения операций:
-res1: Джаз, Блюз
-res2: Джаз, Блюз, Рок-н-ролл
-res3: Джаз, Классика, Рок-н-ролл
-res4: Классика, Рок-н-ролл
-res5: Рэп, Регги, Классика, Рок-н-ролл
-res6:: * Рэп, * Регги, * Классика, * Рок-н-ролл
-*/
-
-let styles = ['jazz', 'blues']
-const res1 = [...styles];
-styles.push('rock n roll')
-const res2 = [...styles]
-function addClassicInMiddleArrayPart(array) {
-array[(Math.ceil(array.length / 2)) - 1] = 'classic'
-return array;
-}
-const res3 = addClassicInMiddleArrayPart(styles);
-
-const res4 = styles.shift();
-styles.unshift('rap', 'reggae')
-const res5 = [...styles]
-addStarsInOddArrayElements(styles)
-const res6 = styles
-function addStarsInOddArrayElements(array){
-let i = 0
-let trigger = true;
-while(i < array.length - 1){
-
-if ((i + 1) % 2 !== 0){
-array[i] = '*' + array[i]
-}
-if (i === array.length - 1){
-trigger = false;
-}
-i++;
-}
-}
-console.info('res1 ' + res1)
-console.info('res2 ' + res2)
-console.info('res3 ' + res3)
-console.info('res4 ' + res4)
-console.info('res5 ' + res5)
-console.info('res6 ' + res6)
+    */
+    const a1=["Аня","Алена","Бося","Мама","Папа"]
+    const x1 = a1.indexOf("Мама")
+    const a2 = ["Бабушка","Дедушка"]   
+    const x2 = [...a1, "Бабушка","Дедушка"]
+    const x3 = [...x2];
+    x3.sort();
+    const x4 = [...x3];
+    x4.splice(3, 1)
+    const x5 = "моя семья - " + a1
+    const x6 = x5.length
+    
+console.info(x1,x2,x3,x4,x5,x6)
