45da92b227250a1b3c392269f87fe57c22ab970b
Dmirtii <smiddist@gmail.com>
2022-08-22 19:46:20 +0300

    add taks090.js

---
+++ task090.js
@@ -0,0 +1,29 @@
+const arr = [
+    { class: 'second', fare: 'a', weight: 12 },
+    { class: 'second', fare: 'b', weight: 10 },
+    { class: 'first', fare: 'a', weight: 15 },
+    { class: 'first', fare: 'a', weight: 17 },
+    { class: 'second', fare: 'a', weight: 12 },
+    { class: 'first', fare: 'c', weight: 30 },
+    { class: 'second', fare: 'b', weight: 22 },
+  ]
+  
+  function arrayPlus(array, prop){
+      const resFin = []
+      const res = array.map(function(item){
+          return item.class+item.fare
+      })
+      const res1 = res.filter(function (item, index){
+          return  index === res.indexOf(item)
+      })
+      for (let i = 0; i < res1.length; i++){
+          for (let j = 0; j < array.length; j++){
+            if (array[j].class + array[j].fare === res1[i]){
+            resFin[i] = array[j]
+           }
+      }
+  }   
+          return resFin
+  }
+  const a = arrayPlus(arr,["class", "fare"])
+  console.info(a)
